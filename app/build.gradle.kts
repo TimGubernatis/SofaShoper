5b6d2a471632d2a01f4c913ad2f5fa41707320a0
TimGubernatis <guberto@kabelmail.de>
2025-06-12 15:58:15 +0200

    Update minimum SDK version to 28
    The minimum SDK version for the application has been increased from 26 to 28.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "de.syntax_institut.androidabschlussprojekt"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
