3f18c17b36128de83c2765b0bf829a76cfa35728
TimGubernatis <guberto@kabelmail.de>
2025-06-26 09:06:53 +0200

    Remove unused import
    The `androidx.compose.ui.draw.scale` import was removed from `DealOfTheDayBanner.kt` as it was no longer in use.

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen_home/components/DealOfTheDayBanner.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/ui/screen_home/components/DealOfTheDayBanner.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
-import androidx.compose.ui.draw.scale
+
 
 @Composable
 fun DealOfTheDayBanner() {
